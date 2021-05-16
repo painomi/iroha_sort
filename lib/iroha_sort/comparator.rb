bcdcc780d6809986c374f222c30a7d760eccd9d9
Painomi Ringo <painomi.ringo@gmail.com>
2021-05-16 15:59:59 +0900

    コアロジック実装

--- lib/iroha_sort/comparator.rb
+++ lib/iroha_sort/comparator.rb
@@ -3,5 +3,22 @@ module IrohaSort
     def initialize(iroha)
       @iroha= iroha
     end
+
+    def compare(a, b)
+      res= 0
+      ary_a, ary_b= a.chars, b.chars
+      size= [ary_a.length, ary_b.length].min
+
+      size.times do |i|
+        res= @iroha.index(ary_a[i]) <=> @iroha.index(ary_b[i])
+        break unless res==0
+      end
+
+      if res== 0
+        res= ary_a.length <=> ary_b.length
+      end
+
+      return res
+    end
   end
 end
--- spec/iroha_sort/comparator_spec.rb
+++ spec/iroha_sort/comparator_spec.rb
@@ -1,3 +1,21 @@
 RSpec.describe IrohaSort::Comparator do
-  it "can initialize."
+  let(:iroha) { "いろはにほへと ちりぬるを わかよたれそ つねならむ うゐのおくやま けふこえて あさきゆめみし ゑひもせす".gsub(/\s/,'') }
+
+  it { expect(IrohaSort::Comparator.new(iroha)).to be_an_instance_of(IrohaSort::Comparator) }
+
+  describe "#compare" do
+    let(:comparator){ IrohaSort::Comparator.new(iroha) }
+
+    it "第1引数と第2引数が同じなら0" do
+      expect(comparator.compare('い', 'い')).to eq(0)
+    end
+
+    it "第1引数が大きいなら1" do
+      expect(comparator.compare('い', 'ろ')).to eq(-1)
+    end
+
+    it "第2引数が大きいなら-1" do
+      expect(comparator.compare('ろ', 'い')).to eq(1)
+    end
+  end
 end
