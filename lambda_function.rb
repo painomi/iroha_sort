f705ae2539330bb2b94ca4e4e646ffea7acd40ba
Painomi Ringo <painomi.ringo@gmail.com>
2021-07-24 15:48:59 +0900

    APIデプロイ成功した版

--- lambda_function.rb
+++ lambda_function.rb
@@ -2,13 +2,12 @@ require 'json'
 require './lib/iroha_sort.rb'
 
 def lambda_handler(event:, context:)
-  iroha= context['iroha']
-  targets= context['targets']
+  iroha=   event["iroha"]
+  targets= event["targets"]
   result= {
     iroha: iroha,
     targets: targets,
     sorted: IrohaSort.sort(iroha, targets)
   }
-
   { statusCode: 200, body: JSON.generate(result) }
 end
